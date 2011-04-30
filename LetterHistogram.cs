5987e32fd72f4de9cd32089549d31b826ed39b35
dreadwail <dreadwail42@gmail.com>
2011-04-30 02:18:02 -0700

    Initial commit of LetterHistogram to git.

---
+++ LetterHistogram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LetterHistogram
+{
+    class LetterHistogram
+    {
+        private string input;
+        private int largestCount;
+        private Dictionary<char, int> counts;
+
+        public LetterHistogram(string input)
+        {
+            this.input = input;
+            this.counts = new Dictionary<char, int>();
+        }
+
+        public void Process()
+        {
+            foreach (char c in input)
+            {
+                if (counts.ContainsKey(c))
+                {
+                    counts[c]++;
+                    if (counts[c] > largestCount)
+                        largestCount = counts[c];
+                }
+                else
+                {
+                    counts.Add(c, 1);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            for (int i = largestCount; i > 0; i--)
+            {
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    if (counts.ContainsKey(c) && counts[c] >= i)
+                    {
+                        stringBuilder.Append("*");
+                    }
+                    else
+                    {
+                        stringBuilder.Append(" ");
+                    }
+                }
+
+                stringBuilder.AppendLine();
+            }
+
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                stringBuilder.Append(c);
+            }
+
+            stringBuilder.AppendLine();
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LetterHistogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+
+            string toParse = string.Concat(args);
+
+            var histogram = new LetterHistogram(toParse);
+            histogram.Process();
+
+            Console.WriteLine(histogram);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Letter Histogram! Prints a histogram representing the count of each letter in a string.");
+            Console.WriteLine("Usage: " + AppDomain.CurrentDomain.FriendlyName + " <string>");
+        }
+    }
+}
