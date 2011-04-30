5351cc88ccb20ef628d82e4c652dafb1e5394586
dreadwail <dreadwail42@gmail.com>
2011-04-30 01:16:50 -0700

    Initial commit of CashRegister to git.

---
+++ CashRegister.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace CashRegister
+{
+    partial class CashRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CashRegister));
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gbxInput = new System.Windows.Forms.GroupBox();
+            this.lblTendered = new System.Windows.Forms.Label();
+            this.txtTendered = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.gbxOutput = new System.Windows.Forms.GroupBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.menu.SuspendLayout();
+            this.gbxInput.SuspendLayout();
+            this.gbxOutput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(334, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // gbxInput
+            // 
+            this.gbxInput.Controls.Add(this.lblTendered);
+            this.gbxInput.Controls.Add(this.txtTendered);
+            this.gbxInput.Controls.Add(this.lblPrice);
+            this.gbxInput.Controls.Add(this.txtPrice);
+            this.gbxInput.Location = new System.Drawing.Point(13, 28);
+            this.gbxInput.Name = "gbxInput";
+            this.gbxInput.Size = new System.Drawing.Size(150, 155);
+            this.gbxInput.TabIndex = 1;
+            this.gbxInput.TabStop = false;
+            this.gbxInput.Text = "Input";
+            // 
+            // lblTendered
+            // 
+            this.lblTendered.AutoSize = true;
+            this.lblTendered.Location = new System.Drawing.Point(19, 49);
+            this.lblTendered.Name = "lblTendered";
+            this.lblTendered.Size = new System.Drawing.Size(53, 13);
+            this.lblTendered.TabIndex = 3;
+            this.lblTendered.Text = "Tendered";
+            this.lblTendered.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtTendered
+            // 
+            this.txtTendered.Location = new System.Drawing.Point(78, 46);
+            this.txtTendered.Name = "txtTendered";
+            this.txtTendered.Size = new System.Drawing.Size(66, 20);
+            this.txtTendered.TabIndex = 2;
+            this.txtTendered.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtTendered.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTendered_KeyPress);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(41, 22);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(31, 13);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Price";
+            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(78, 19);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(66, 20);
+            this.txtPrice.TabIndex = 0;
+            this.txtPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrice_KeyPress);
+            // 
+            // gbxOutput
+            // 
+            this.gbxOutput.Controls.Add(this.lblOutput);
+            this.gbxOutput.Location = new System.Drawing.Point(169, 28);
+            this.gbxOutput.Name = "gbxOutput";
+            this.gbxOutput.Size = new System.Drawing.Size(150, 155);
+            this.gbxOutput.TabIndex = 2;
+            this.gbxOutput.TabStop = false;
+            this.gbxOutput.Text = "Output";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(7, 20);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 189);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(150, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(168, 189);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(149, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // CashRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 224);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.gbxOutput);
+            this.Controls.Add(this.gbxInput);
+            this.Controls.Add(this.menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menu;
+            this.MaximizeBox = false;
+            this.Name = "CashRegister";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Cash Register";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.gbxInput.ResumeLayout(false);
+            this.gbxInput.PerformLayout();
+            this.gbxOutput.ResumeLayout(false);
+            this.gbxOutput.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.GroupBox gbxInput;
+        private System.Windows.Forms.GroupBox gbxOutput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblTendered;
+        private System.Windows.Forms.TextBox txtTendered;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ CashRegister.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CashRegister
+{
+    public partial class CashRegister : Form
+    {
+        public CashRegister()
+        {
+            this.InitializeComponent();
+            this.Reset();
+        }
+
+        private void Reset()
+        {
+            this.txtPrice.Text = "";
+            this.txtTendered.Text = "";
+            this.lblOutput.Text = "";
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Reset();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A simple change-making cash register application." +
+                Environment.NewLine +
+                Environment.NewLine +
+                "Written by Ben Lakey.", "About", MessageBoxButtons.OK);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            this.Reset();
+        }
+
+        private void txtPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (this.IsAllowedChar(e.KeyChar) == false || e.KeyChar.ToString() == "." && this.txtPrice.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTendered_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (this.IsAllowedChar(e.KeyChar) == false || e.KeyChar.ToString() == "." && this.txtTendered.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool IsAllowedChar(char suspect)
+        {
+            if (char.IsDigit(suspect) || suspect == '.' || Char.IsControl(suspect))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (this.txtTendered.Text.Length == 0)
+            {
+                this.txtTendered.Text = "0.00";
+            }
+
+            if (this.txtPrice.Text.Length == 0)
+            {
+                this.txtPrice.Text = "0.00";
+            }
+
+            double tendered = 0.00;
+            double price = 0.00;
+
+            double result = 0.0;
+            if (double.TryParse(txtTendered.Text, out result) == true)
+            {
+                tendered = result;
+            }
+            
+            result = 0.0;
+            if (double.TryParse(txtPrice.Text, out result) == true)
+            {
+                price = result;
+            }
+            
+            List<ChangeMaker.Coin> change = ChangeMaker.Calculate(tendered, price);
+
+            var toReturn = new Dictionary<ChangeMaker.Coin, int>();
+            foreach (ChangeMaker.Coin thisDenom in change)
+            {
+                if (toReturn.ContainsKey(thisDenom))
+                {
+                    toReturn[thisDenom]++;
+                }
+                else
+                {
+                    toReturn.Add(thisDenom, 1);
+                }
+            }
+
+            string total = String.Format("{0:C}", ChangeMaker.DenominationsToDollarAmount(change));
+
+            this.lblOutput.Text = "";
+            this.lblOutput.Text += "Total: " + total + Environment.NewLine;
+            this.lblOutput.Text += Environment.NewLine;
+            this.lblOutput.Text += "Return:" + Environment.NewLine;
+
+            foreach (KeyValuePair<ChangeMaker.Coin, int> keyval in toReturn)
+            {
+                this.lblOutput.Text += keyval.Value + " of " + keyval.Key.ToString() + Environment.NewLine;
+            }
+
+        }
+    }
+}
