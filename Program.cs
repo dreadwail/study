aa01eab1d01d338ba4509d6e4bd08dd687d60a94
dreadwail <dreadwail42@gmail.com>
2011-04-30 15:55:25 -0700

    Initial commit of Zorro to git.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Zorro
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                PrintUsage();
+            }
+            else
+            {
+                int result = 0;
+                if (Int32.TryParse(args[0], out result) == false)
+                {
+                    Console.WriteLine("Unable to parse input integer.");
+                }
+                else
+                {
+                    var zorro = new Zorro(result);
+                    Console.WriteLine(zorro);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Zorro! A program to print a Z using the alphabet.");
+            Console.WriteLine("Usage: {0} <width of Z as integer>", AppDomain.CurrentDomain.FriendlyName);
+        }
+    }
+}
---
+++ Zorro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zorro
+{
+    public class Zorro
+    {
+        private int size;
+
+        public Zorro(int size)
+        {
+            this.size = size;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine();
+
+            char c = 'z';
+            int posX = 0;
+
+            for (posX = 0; posX < this.size; posX++)
+            {
+                stringBuilder.Append(c = (c == 'z' ? 'a' : ++c));
+            }
+
+            stringBuilder.AppendLine();
+
+            for (posX = this.size - 2; posX > 0; posX--)
+            {
+                for (int i = 0; i < posX; i++)
+                {
+                    stringBuilder.Append(" ");
+                }
+
+                stringBuilder.AppendLine((c = (c == 'z' ? 'a' : ++c)).ToString());
+            }
+
+            for (posX = 0; posX < this.size; posX++)
+            {
+                stringBuilder.Append(c = (c == 'z' ? 'a' : ++c));
+            }
+
+            stringBuilder.AppendLine();
+
+            return stringBuilder.ToString();
+        }
+    }
+}
