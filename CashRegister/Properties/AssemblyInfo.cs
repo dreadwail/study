829f3db304173963b65b9310fcb58356c33f2b89
dreadwail <dreadwail42@gmail.com>
2011-05-29 23:03:18 -0700

    Reorganized structure, added README

--- CashRegister/Properties/AssemblyInfo.cs
+++ CashRegister/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CashRegister")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple cash register in C#.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("bytesrc.com")]
+[assembly: AssemblyCompany("Ben Lakey")]
 [assembly: AssemblyProduct("CashRegister")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © Ben Lakey 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
