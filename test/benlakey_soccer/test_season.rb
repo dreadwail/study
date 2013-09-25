116cb9f5ba8ee221fa46830578f3b0b09922dc5c
dreadwail <dreadwail42@gmail.com>
2013-09-24 23:54:41 -0700

    Testing against substantial provided test data.

--- test/benlakey_soccer/test_season.rb
+++ test/benlakey_soccer/test_season.rb
@@ -69,5 +69,28 @@ eos
     assert_equal expected, @season.to_s
   end
 
+  def test_substantial_sample_data
+    SUBSTANTIAL_SAMPLE_INPUT.split("\n").each do |game|
+      @season.record_game(game)
+    end
+    assert_equal SUBSTANTIAL_SAMPLE_OUTPUT, @season.to_s
+  end
+
+  SUBSTANTIAL_SAMPLE_INPUT = <<-EOF
+Lions 3, Snakes 3
+Tarantulas 1, FC Awesome 0
+Lions 1, FC Awesome 1
+Tarantulas 3, Snakes 1
+Lions 4, Grouches 0
+  EOF
+
+  SUBSTANTIAL_SAMPLE_OUTPUT = <<-EOF
+1. Tarantulas, 6 pts
+2. Lions, 5 pts
+3. FC Awesome, 1 pt
+3. Snakes, 1 pt
+5. Grouches, 0 pts
+  EOF
+
 end
 
