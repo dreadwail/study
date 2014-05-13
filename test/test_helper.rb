7999db7035222c34f8170f76e03478062cdfe41f
dreadwail <dreadwail42@gmail.com>
2014-05-13 01:18:43 -0700

    Pride

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require "bundler"
 Bundler.setup(ENV["EULER_ENV"])
 
 require "minitest/autorun"
+require "minitest/pride"
 
 lib_path = File.join(File.expand_path(File.dirname(__FILE__)), "..", "lib")
 $: << lib_path unless $:.include? lib_path
