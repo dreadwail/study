6f129980bdb12da422a1dc5054f2acc1d63c0b47
dreadwail <dreadwail42@gmail.com>
2014-05-13 01:19:06 -0700

    Add the actual problem for problem1

--- test/test_problem1.rb
+++ test/test_problem1.rb
@@ -4,8 +4,12 @@ require "problem1"
 class TestEuler < Minitest::Test
   include Euler
 
-  def test_multiples_of_3_and_5
+  def test_small_number
     assert_equal 78, multiples_of_3_and_5(20)
+  end
+
+  def test_actual_problem
+    assert_equal 233168, multiples_of_3_and_5(1000)
   end
 
 end
