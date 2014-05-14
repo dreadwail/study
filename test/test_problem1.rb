bc3ab64eee7a3684b7f7addba69e6332ab26b403
Ben Lakey <benlakey@gmail.com>
2014-05-13 19:19:43 -0700

    Refactor problem 1

--- test/test_problem1.rb
+++ test/test_problem1.rb
@@ -1,15 +1,14 @@
 require "test_helper"
 require "problem1"
 
-class TestEuler < Minitest::Test
-  include Euler
+class TestProblem1 < Minitest::Test
 
   def test_small_number
-    assert_equal 78, multiples_of_3_and_5(20)
+    assert_equal 78, (1...20).sum_multiples_of_3_and_5
   end
 
   def test_actual_problem
-    assert_equal 233168, multiples_of_3_and_5(1000)
+    assert_equal 233168, (1...1000).sum_multiples_of_3_and_5
   end
 
 end
