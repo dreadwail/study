56b0d33bc068c07b4c56852221035cb7e689eb32
Ben Lakey <benlakey@gmail.com>
2014-05-13 22:16:55 -0700

    Refactor problem 9

--- test/test_problem9.rb
+++ test/test_problem9.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 require "problem9"
 
-class TestEuler < Minitest::Test
-  include Euler
+class TestProblem9 < Minitest::Test
+  include Problem9
+
+  def test_small_number
+    assert_equal [3,4,5], pythagorean_triplet_summing_to(12)
+  end
 
   def test_pythagorean_triplet_for_product
     assert_equal 31875000, pythagorean_triplet_summing_to(1000).inject(:*)
