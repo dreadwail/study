d820e0e8d6dee8840aab1a62fac4b0904de8daf8
dreadwail <dreadwail42@gmail.com>
2014-05-13 19:19:43 -0700

    Refactor problem 1

--- lib/problem1.rb
+++ lib/problem1.rb
@@ -1,9 +1,7 @@
-module Euler
+class Range
 
-  def multiples_of_3_and_5(max)
-    (1...max)
-      .select { |n| n % 3 == 0 || n % 5 == 0 }
-      .inject(:+)
+  def sum_multiples_of_3_and_5
+    select { |n| n % 3 == 0 || n % 5 == 0 }.inject(:+)
   end
 
 end
