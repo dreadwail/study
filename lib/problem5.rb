e4268bb08262a334a428bf081ac2a48e1a22bcbf
dreadwail <dreadwail42@gmail.com>
2014-05-13 21:04:33 -0700

    Refactor problem 5

--- lib/problem5.rb
+++ lib/problem5.rb
@@ -1,10 +1,8 @@
-module Euler
+class Range
 
-  def divisible_by_all(range)
-    candidate = range.last
-    until range.all? { |i| candidate % i == 0 }
-      candidate += range.last
-    end
+  def smallest_divisible_by_all
+    candidate = last
+    candidate += last until all? { |i| candidate % i == 0 }
     candidate
   end
 
