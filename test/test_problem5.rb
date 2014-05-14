91e8f22f336e5670091a70f01751cf37b5ec77e6
Ben Lakey <benlakey@gmail.com>
2014-05-13 21:04:33 -0700

    Refactor problem 5

--- test/test_problem5.rb
+++ test/test_problem5.rb
@@ -2,14 +2,13 @@ require "test_helper"
 require "problem5"
 
 class TestEuler < Minitest::Test
-  include Euler
 
   def test_small_problem
-    assert_equal 2520, divisible_by_all(1..10)
+    assert_equal 2520, (1..10).smallest_divisible_by_all
   end
 
   def test_actual_problem
-    assert_equal 232792560, divisible_by_all(1..20)
+    assert_equal 232792560, (1..20).smallest_divisible_by_all
   end
 
 end
