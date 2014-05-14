338cca3710455ed8588dff60f5a165de7f0cb149
Ben Lakey <benlakey@gmail.com>
2014-05-13 19:14:28 -0700

    Extract fib from problem2 since problem25 will need it

--- lib/problem2.rb
+++ lib/problem2.rb
@@ -1,23 +1,15 @@
+require "fib"
+
 module Euler
 
   def sum_even_fibs(max)
-    n = 0
+    n = 1
     sum = 0
-    while (fib_of(n) < max)
-      sum += fib_of(n) if fib_of(n) % 2 == 0
+    while (Fib.fib_of(n) < max)
+      sum += Fib.fib_of(n) if Fib.fib_of(n) % 2 == 0
       n += 1
     end
     sum
-  end
-
-  KNOWN_FIBS = { 0 => 1, 1 => 1 }
-
-  def fib_of(n)
-    return KNOWN_FIBS[n] if KNOWN_FIBS[n]
-    (2..n).each do |i|
-      KNOWN_FIBS[i] = KNOWN_FIBS[i - 1] + KNOWN_FIBS[i - 2]
-    end
-    KNOWN_FIBS[n]
   end
 
 end
