70007b4636b2d62a3f992a899264679243083d4e
dreadwail <dreadwail42@gmail.com>
2015-05-25 14:07:13 -0700

    Romanize numbers

--- lib/algos/numbers.rb
+++ lib/algos/numbers.rb
@@ -1,6 +1,32 @@
 module Algos
   module Numbers
 
+    # TODO: doesnt handle 4 == IV style numerals yet
+    def self.romanize(number)
+      numerals = ""
+
+      cees = number / 100
+      numerals += "C" * cees
+      number -= (cees * 100)
+
+      ells = number / 50
+      numerals += "L" * ells
+      number -= (ells * 50)
+
+      exes = number / 10
+      numerals += "X" * exes
+      number -= (exes * 10)
+
+      vees = number / 5
+      numerals += "V" * vees
+      number -= (vees * 5)
+
+      eyes = number % 5
+      numerals += "I" * eyes
+
+      numerals
+    end
+
     def self.dec2bin(str)
       return nil if !str || str == ""
       num = str.to_i
--- test/test_numbers.rb
+++ test/test_numbers.rb
@@ -2,6 +2,43 @@ require "test_helper"
 
 class TestNumbers < Minitest::Test
 
+  def test_romanize_one
+    assert_equal "I", Algos::Numbers.romanize(1)
+  end
+
+  def test_romanize_ones
+    assert_equal "II", Algos::Numbers.romanize(2)
+  end
+
+  # TODO
+  #def test_romanize_special_ones
+  #  assert_equal "IV", Algos::Numbers.romanize(4)
+  #end
+
+  def test_romanize_ten
+    assert_equal "X", Algos::Numbers.romanize(10)
+  end
+
+  def test_romanize_tens
+    assert_equal "XI", Algos::Numbers.romanize(11)
+  end
+
+  def test_romanize_fifty
+    assert_equal "L", Algos::Numbers.romanize(50)
+  end
+
+  def test_romanize_fifties
+    assert_equal "LI", Algos::Numbers.romanize(51)
+  end
+
+  def test_romanize_hundred
+    assert_equal "C", Algos::Numbers.romanize(100)
+  end
+
+  def test_romanize_hundreds
+    assert_equal "CI", Algos::Numbers.romanize(101)
+  end
+
   def test_dec2bin_nil_gets_nil
     assert_nil Algos::Numbers.dec2bin(nil)
   end
