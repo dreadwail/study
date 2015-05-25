5f311982f17e4073c9e676a5316f0391250148aa
dreadwail <dreadwail42@gmail.com>
2015-05-25 13:51:57 -0700

    Decimal to binary

--- lib/algos/numbers.rb
+++ lib/algos/numbers.rb
@@ -1,6 +1,17 @@
 module Algos
   module Numbers
 
+    def self.dec2bin(str)
+      return nil if !str || str == ""
+      num = str.to_i
+      bin = ""
+      while num > 0
+        bin = (num % 2).to_s + bin
+        num /= 2
+      end
+      bin
+    end
+
     def self.bin2dec(str)
       return nil if !str || str == ""
       place = 1
--- test/test_numbers.rb
+++ test/test_numbers.rb
@@ -2,6 +2,30 @@ require "test_helper"
 
 class TestNumbers < Minitest::Test
 
+  def test_dec2bin_nil_gets_nil
+    assert_nil Algos::Numbers.dec2bin(nil)
+  end
+
+  def test_dec2bin_empty_gets_nil
+    assert_nil Algos::Numbers.dec2bin("")
+  end
+
+  def test_dec2bin_zero_gets_zero
+    assert_equal "", Algos::Numbers.dec2bin("0")
+  end
+
+  def test_dec2bin_one_gets_one
+    assert_equal "1", Algos::Numbers.dec2bin("1")
+  end
+
+  def test_dec2bin_all_ones
+    assert_equal "11111", Algos::Numbers.dec2bin("31")
+  end
+
+  def test_dec2bin
+    assert_equal "10001", Algos::Numbers.dec2bin("17")
+  end
+
   def test_bin2dec_nil_gets_nil
     assert_nil Algos::Numbers.bin2dec(nil)
   end
