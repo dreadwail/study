680b7c8093df35ea5090ba6c29197d601a15fdc9
dreadwail <dreadwail42@gmail.com>
2015-07-06 20:02:27 -0700

    Correct naming of tests to correspond to method name.

--- test/algos/strings/test_string_calculator.rb
+++ test/algos/strings/test_string_calculator.rb
@@ -6,54 +6,54 @@ class TestStringCalculator < Minitest::Test
     @calculator = Algos::Strings::Calculator.new
   end
 
-  def test_calculate_with_empty_string
+  def test_add_with_empty_string
     @calculator.add("")
     assert_equal 0, @calculator.value
   end
 
-  def test_calculate_with_one_numbers
+  def test_add_with_one_numbers
     @calculator.add("4")
     assert_equal 4, @calculator.value
   end
 
-  def test_calculate_with_two_numbers
+  def test_add_with_two_numbers
     @calculator.add("4", "2")
     assert_equal 6, @calculator.value
   end
 
-  def test_calculate_with_one_number_one_empty
+  def test_add_with_one_number_one_empty
     @calculator.add("4", "")
     assert_equal 4, @calculator.value
   end
 
-  def test_calculate_with_all_empty
+  def test_add_with_all_empty
     @calculator.add("", "")
     assert_equal 0, @calculator.value
   end
 
-  def test_calculate_with_three_numbers
+  def test_add_with_three_numbers
     @calculator.add("2", "4", "6")
     assert_equal 12, @calculator.value
   end
 
-  def test_calculate_with_addition_of_ten
+  def test_add_with_addition_of_ten
     @calculator.add("10")
     assert_equal 10, @calculator.value
   end
 
-  def test_calculate_with_unknown_number_of_numbers
+  def test_add_with_unknown_number_of_numbers
     numbers = Random.new.rand(1..20).times.map { Random.new.rand(1..20) }
     actual_sum = numbers.inject(:+)
     @calculator.add(*numbers.map(&:to_s))
     assert_equal actual_sum, @calculator.value
   end
 
-  def test_calculate_one_negative
+  def test_add_one_negative
     @calculator.add("-2")
     assert_equal -2, @calculator.value
   end
 
-  def test_calculate_two_negatives
+  def test_add_two_negatives
     @calculator.add("-2", "-6")
     assert_equal -8, @calculator.value
   end
