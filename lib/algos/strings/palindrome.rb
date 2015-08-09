f88c68fb359fe1fe531c0c7238e02bca1fcb62c9
dreadwail <dreadwail42@gmail.com>
2015-08-09 15:56:48 -0700

    Greatly simplify palindrome check.

--- lib/algos/strings/palindrome.rb
+++ lib/algos/strings/palindrome.rb
@@ -1,14 +1,7 @@
 class String
 
   def palindrome?
-    left = 0
-    right = length - 1
-    while left < right
-      return false if self[left] != self[right]
-      left += 1
-      right -= 1
-    end
-    true
+    (0..length/2).all? { |i| self[i] == self[length - 1 - i] }
   end
 
 end
