d742de716353f9ec82e01bff9d1a8d3e7e0fca32
dreadwail <dreadwail42@gmail.com>
2015-11-28 20:47:37 -0800

    Change problem 1 to use ruby refinements on a range.

--- lib/euler/problem_001.rb
+++ lib/euler/problem_001.rb
@@ -1,9 +1,11 @@
 module Euler
-  
-  def find_sum_multiples(*multipliers, max:)
-    (1...max).select do |candidate|
-      multipliers.any? { |m| candidate % m == 0 }
-    end.inject(:+)
+
+  refine Range do
+    def find_sum_multiples(*multipliers)
+      select do |candidate|
+        multipliers.any? { |m| candidate % m == 0 }
+      end.inject(:+)
+    end
   end
 
 end
