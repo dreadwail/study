f4c355982928316eb5a8ea4f8eb414128b17cfb4
Ben Lakey <benlakey@gmail.com>
2016-06-04 21:46:25 -0700

    Initial commit

---
+++ test_checkout.rb
@@ -0,0 +1,49 @@
+require "minitest/autorun"
+require "minitest/pride"
+require_relative "./checkout"
+require_relative "./rule"
+
+class TestCheckout < Minitest::Test
+
+  RULES = [
+    Rule.new("A", 3, 130),
+    Rule.new("B", 2, 45),
+    Rule.new("B", 1, 30),
+    Rule.new("C", 1, 20),
+    Rule.new("D", 1, 15)
+  ]
+
+  def price(goods)
+    co = Checkout.new(RULES)
+    goods.split(//).each { |item| co.scan(item) }
+    co.total
+  end
+
+  def test_totals
+    assert_equal(  0, price(""))
+    assert_equal( 50, price("A"))
+    assert_equal( 80, price("AB"))
+    assert_equal(115, price("CDBA"))
+
+    assert_equal(100, price("AA"))
+    assert_equal(130, price("AAA"))
+    assert_equal(180, price("AAAA"))
+    assert_equal(230, price("AAAAA"))
+    assert_equal(260, price("AAAAAA"))
+
+    assert_equal(160, price("AAAB"))
+    assert_equal(175, price("AAABB"))
+    assert_equal(190, price("AAABBD"))
+    assert_equal(190, price("DABABA"))
+  end
+
+  def test_incremental
+    co = Checkout.new(RULES)
+    assert_equal(  0, co.total)
+    co.scan("A");  assert_equal( 50, co.total)
+    co.scan("B");  assert_equal( 80, co.total)
+    co.scan("A");  assert_equal(130, co.total)
+    co.scan("A");  assert_equal(160, co.total)
+    co.scan("B");  assert_equal(175, co.total)
+  end
+end
