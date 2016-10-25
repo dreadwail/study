8a77e0ff0001fb791be7d5e5fd96269ca143b564
Ben Lakey <blakey@yammer-inc.com>
2016-10-24 22:58:36 -0700

    Give the frame some smarts

--- frame.rb
+++ frame.rb
@@ -1,10 +1,28 @@
 class Frame
 
+  PIN_COUNT = 10
+
+  attr_accessor :rolls
+
   def initialize
     @rolls = []
   end
 
-  attr_accessor :rolls
+  def open?
+    !closed?
+  end
+
+  def closed?
+    pins_down_count == PIN_COUNT || @rolls.length == 2
+  end
+
+  def pins_down_count
+    @rolls.inject(:+)
+  end
+
+  def pins_up_count
+    PIN_COUNT - pins_down_count
+  end
 
 end
 
