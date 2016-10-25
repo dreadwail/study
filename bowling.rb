7c9bfd1ceeb89d15a7a147dd1868ad8c7c27f582
Ben Lakey <blakey@yammer-inc.com>
2016-10-25 00:10:04 -0700

    Give frames the ability to look-ahead and score strikes/spares.

--- bowling.rb
+++ bowling.rb
@@ -2,23 +2,38 @@ require_relative "./frame"
 
 class Game
 
+  FRAME_COUNT = 10
+
   def initialize
-    @frames = Array.new(10) { Frame.new }
-    @frame_index = 0
+    tail_frame = Frame.new
+
+    FRAME_COUNT.times { tail_frame = Frame.new(tail_frame) }
+
+    @head_frame = tail_frame
+    @current_frame = tail_frame
   end
 
   def roll(pins)
-    frame = @frames[@frame_index]
-    frame.rolls << pins
-    if frame.closed?
-      @frame_index += 1
+    @current_frame.rolls << pins
+    if @current_frame.closed?
+      @current_frame = @current_frame.next_frame
     end
   end
 
   def score
-    @frames
-      .flat_map(&:rolls)
-      .inject(:+)
+    frames
+      .flat_map(&:score)
+      .inject(0, :+)
+  end
+
+  def frames
+    [].tap do |list|
+      this_frame = @head_frame
+      while this_frame
+        list << this_frame
+        this_frame = this_frame.next_frame
+      end
+    end
   end
 
 end
