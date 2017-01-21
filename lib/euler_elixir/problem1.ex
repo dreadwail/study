06383c7d6ec0a24c4eb0153d7c129847de2a527a
dreadwail <blakey@yammer-inc.com>
2017-01-21 14:35:46 -0800

    Rework problem1 to be cleaner and cleanup the project structure.

---
+++ lib/euler_elixir/problem1.ex
@@ -0,0 +1,20 @@
+defmodule EulerElixir do
+  defmodule Problem1 do
+
+    def sum_of_multiples(less_than: max) do
+      0..(max - 1)
+        |> Enum.filter(&valid_number?/1)
+        |> Enum.sum
+    end
+
+    defp valid_number?(number) do
+      divisible?(number, 3) || divisible?(number, 5)
+    end
+
+    defp divisible?(number, by) do
+      rem(number, by) == 0
+    end
+
+  end
+end
+
