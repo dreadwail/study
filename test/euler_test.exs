908e03c76b8c893ac7e1a3eb44019ad63eb9d2f5
dreadwail <dreadwail42@gmail.com>
2017-01-21 14:35:46 -0800

    Rework problem1 to be cleaner and cleanup the project structure.

--- test/euler_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule EulerTest do
-  use ExUnit.Case
-  doctest Euler
-
-  test "problem1 - can find sum of multiples below 10" do
-    sum = Euler.Problem1.sum_of_multiples(less_than: 10)
-    assert sum == 23
-  end
-end
