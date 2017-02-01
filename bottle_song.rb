cb8b116b1a5254f04cd63f0d85ee7f8511b82a1b
Ben Lakey <blakey@yammer-inc.com>
2017-02-01 13:31:08 -0800

    Can sing verse 99

---
+++ bottle_song.rb
@@ -0,0 +1,18 @@
+class BottleSong
+
+  def initialize
+    
+  end
+
+  def verse(n)
+    [
+      "99 bottles of beer on the wall, 99 bottles of beer.",
+      "Take one down and pass it around, 98 bottles of beer on the wall."
+    ].join("\n")
+  end
+
+  def self.sing
+    
+  end
+
+end
