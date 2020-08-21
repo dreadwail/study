99f424f604c93b4fafeb92ab3361af201deafbcf
dreadwail <dreadwail42@gmail.com>
2020-08-21 16:10:01 -0700

    WIP

--- src/support/arrays.ts
+++ src/support/arrays.ts
@@ -1,1 +1,14 @@
-export const generateSequence = (length: number) => Array.from(Array(length).keys());
+type GenerateSequenceOptions = {
+  readonly max: number;
+  readonly inclusive: boolean;
+  readonly startsAt: number;
+};
+
+// switch to generator
+export const generateSequence = ({ max, startsAt, inclusive }: GenerateSequenceOptions) => {
+  if (max < startsAt) {
+    return [];
+  }
+  const length = inclusive ? max + 1 - startsAt : max - startsAt;
+  return Array.from(Array(length).keys()).map((val) => startsAt + val);
+};
