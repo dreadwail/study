34cf35066a5d2381a7b97395ffc1c98cfef7afb4
dreadwail <dreadwail42@gmail.com>
2020-12-05 15:58:57 -0800

    Improve the time complexity of advent of code 2020 december 1 solution from n^3 to n^2.

--- typescript/src/adventofcode2020/dec1.ts
+++ typescript/src/adventofcode2020/dec1.ts
@@ -1,28 +1,45 @@
 export const productOfTwoSumEqualTo = (sum: number, input: number[]): number => {
-  for (let a = 0; a < input.length; a += 1) {
-    for (let b = 0; b < input.length; b += 1) {
-      const numA = input[a];
-      const numB = input[b];
-      if (numA + numB === sum) {
-        return numA * numB;
-      }
+  const set = new Set(input);
+
+  for (let i = 0; i < input.length; i += 1) {
+    const numberHere = input[i];
+    const numberNeeded = sum - numberHere;
+    if (set.has(numberNeeded)) {
+      return numberHere * numberNeeded;
     }
   }
+
   return 0;
 };
 
 export const productOfThreeSumEqualTo = (sum: number, input: number[]): number => {
-  for (let a = 0; a < input.length; a += 1) {
-    for (let b = 0; b < input.length; b += 1) {
-      for (let c = 0; c < input.length; c += 1) {
-        const numA = input[a];
-        const numB = input[b];
-        const numC = input[c];
-        if (numA + numB + numC === sum) {
-          return numA * numB * numC;
-        }
-      }
+  const tuples: number[][] = [];
+
+  for (let number1index = 0; number1index < input.length; number1index += 1) {
+    const number1 = input[number1index];
+    for (let number2index = 0; number2index < input.length; number2index += 1) {
+      const number2 = input[number2index];
+      tuples.push([number1, number2]);
     }
   }
+
+  const sumsToTuples = new Map<number, number[]>();
+
+  for (let tupleIndex = 0; tupleIndex < tuples.length; tupleIndex += 1) {
+    const tuple = tuples[tupleIndex];
+    const sum = tuple[0] + tuple[1];
+    sumsToTuples.set(sum, tuple);
+  }
+
+  for (let number3index = 0; number3index < input.length; number3index += 1) {
+    const numberHere = input[number3index];
+    const numberNeeded = sum - numberHere;
+
+    if (sumsToTuples.has(numberNeeded)) {
+      const tuple = sumsToTuples.get(numberNeeded) || [];
+      return [...tuple, numberHere].reduce((product, current) => product * current);
+    }
+  }
+
   return 0;
 };
