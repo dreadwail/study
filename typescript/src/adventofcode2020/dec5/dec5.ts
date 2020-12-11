04ab1b90280b55c5d806c2838038b8651927f10d
dreadwail <dreadwail42@gmail.com>
2020-12-10 19:01:59 -0800

    Solved advent2020 dec5 for Ben's dataset.

---
+++ typescript/src/adventofcode2020/dec5/dec5.ts
@@ -0,0 +1,56 @@
+type SearchOptions = {
+  instructions: string[];
+  lowerSymbol: string;
+  upperSymbol: string;
+  startingMin: number;
+  startingMax: number;
+};
+
+const search = ({ instructions, lowerSymbol, upperSymbol, startingMax, startingMin }: SearchOptions): number => {
+  const { min } = instructions.reduce(
+    ({ min, max }, instruction) => {
+      const spanLength = max - min;
+      const halfSpanLength = spanLength / 2;
+      const midPoint = min + halfSpanLength;
+      if (instruction === lowerSymbol) {
+        return { min, max: Math.floor(midPoint) };
+      }
+      if (instruction === upperSymbol) {
+        return { min: Math.ceil(midPoint), max };
+      }
+      return { min, max };
+    },
+    { min: startingMin, max: startingMax }
+  );
+
+  return min;
+};
+
+const calculateSeatId = (rowNumber: number, columnNumber: number): number => rowNumber * 8 + columnNumber;
+
+const computeSeatId = (row: string): number => {
+  const instructions = row.split('');
+
+  const rowInstructions = instructions.slice(0, 7);
+  const columnInstructions = instructions.slice(-3);
+
+  const rowNumber = search({
+    instructions: rowInstructions,
+    lowerSymbol: 'F',
+    upperSymbol: 'B',
+    startingMin: 0,
+    startingMax: 127,
+  });
+
+  const columnNumber = search({
+    instructions: columnInstructions,
+    lowerSymbol: 'L',
+    upperSymbol: 'R',
+    startingMin: 0,
+    startingMax: 7,
+  });
+
+  return calculateSeatId(rowNumber, columnNumber);
+};
+
+export const determineHighestSeatId = (rows: string[]): number => Math.max(...rows.map(computeSeatId));
