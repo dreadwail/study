fbc6443b6d3de91894d4a9fbbfabb8c81137c360
dreadwail <dreadwail42@gmail.com>
2021-02-06 14:27:51 -0800

    Fix a bug in the excelSheetColumnNumber leetcode problem that made it fail for inputs 3 chars or larger.

--- typescript/src/leetcode/excelSheetColumnNumber/excelSheetColumnNumber.test.ts
+++ typescript/src/leetcode/excelSheetColumnNumber/excelSheetColumnNumber.test.ts
@@ -5,6 +5,7 @@ describe('titleToNumber', () => {
     [1, 'A'],
     [28, 'AB'],
     [701, 'ZY'],
+    [703, 'AAA'],
   ])('returns %i for %s', (expected, input) => {
     expect(titleToNumber(input)).toEqual(expected);
   });
