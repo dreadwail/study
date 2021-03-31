fd8d7e00e135bf50d0eb39ebdf4c1baf687a1d90
dreadwail <dreadwail42@gmail.com>
2021-03-31 14:17:59 -0700

    Add additional tests cases for lowerCommonAncestor.

--- typescript/src/leetcode/lowestCommonAncestor/lowestCommonAncestor.test.ts
+++ typescript/src/leetcode/lowestCommonAncestor/lowestCommonAncestor.test.ts
@@ -27,4 +27,39 @@ describe('lowestCommonAncestor', () => {
 
     expect(lowestCommonAncestor(three, five, one)).toEqual(three);
   });
+
+  it('returns the correct answer for example 2', () => {
+    const three = new Node(3);
+    const five = new Node(5);
+    const one = new Node(1);
+    const six = new Node(6);
+    const two = new Node(2);
+    const zero = new Node(0);
+    const eight = new Node(8);
+    const seven = new Node(7);
+    const four = new Node(4);
+
+    three.left = five;
+    three.right = one;
+
+    five.left = six;
+    five.right = two;
+
+    one.left = zero;
+    one.right = eight;
+
+    two.left = seven;
+    two.right = four;
+
+    expect(lowestCommonAncestor(three, five, four)).toEqual(five);
+  });
+
+  it('returns the correct answer for example 3', () => {
+    const one = new Node(1);
+    const two = new Node(2);
+
+    one.left = two;
+
+    expect(lowestCommonAncestor(one, one, two)).toEqual(one);
+  });
 });
