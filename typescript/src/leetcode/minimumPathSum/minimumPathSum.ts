fb4166075edc98ca6989d4e2244b01804de0851a
dreadwail <dreadwail42@gmail.com>
2022-03-04 13:16:12 -0800

    Optimize leetcode min path sum problem with dynamic programming cache.

--- typescript/src/leetcode/minimumPathSum/minimumPathSum.ts
+++ typescript/src/leetcode/minimumPathSum/minimumPathSum.ts
@@ -1,10 +1,24 @@
-const minPathSumRecursive = (grid: number[][], x: number, y: number): number => {
+const cacheKeyFor = (x: number, y: number) => [x, y].join(',');
+
+const minPathSumRecursive = (grid: number[][], x: number, y: number, cache: Record<string, number>): number => {
   const destinationX = grid[0].length - 1;
   const destinationY = grid.length - 1;
   const valueHere = grid[y][x];
 
+  const cacheRead = () => cache[cacheKeyFor(x, y)];
+
+  const cacheWrite = (value: number) => {
+    cache[cacheKeyFor(x, y)] = value;
+    return value;
+  };
+
+  const fromCache = cacheRead();
+  if (fromCache != null) {
+    return fromCache;
+  }
+
   if (x === destinationX && y === destinationY) {
-    return valueHere;
+    return cacheWrite(valueHere);
   }
 
   const isDownPossible = y + 1 < grid.length;
@@ -13,13 +27,14 @@ const minPathSumRecursive = (grid: number[][], x: number, y: number): number => 
   const costs: number[] = [];
 
   if (isDownPossible) {
-    costs.push(minPathSumRecursive(grid, x, y + 1));
+    costs.push(minPathSumRecursive(grid, x, y + 1, cache));
   }
   if (isRightPossible) {
-    costs.push(minPathSumRecursive(grid, x + 1, y));
+    costs.push(minPathSumRecursive(grid, x + 1, y, cache));
   }
 
-  return valueHere + Math.min(...costs);
+  const result = valueHere + Math.min(...costs);
+  return cacheWrite(result);
 };
 
-export const minPathSum = (grid: number[][]): number => minPathSumRecursive(grid, 0, 0);
+export const minPathSum = (grid: number[][]): number => minPathSumRecursive(grid, 0, 0, {});
