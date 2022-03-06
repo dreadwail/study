eeb0ae3e5ef98975f4c521374504440a577f859a
dreadwail <dreadwail42@gmail.com>
2022-03-06 13:18:04 -0800

    Solved leetcode redundant graph connections problem again, but this time using a union-find with sets.

--- typescript/src/leetcode/redundantGraphConnection/redundantGraphConnection.ts
+++ typescript/src/leetcode/redundantGraphConnection/redundantGraphConnection.ts
@@ -1,4 +1,4 @@
-export const findRedundantConnection = (edges: [number, number][]): [number, number] => {
+export const findRedundantConnectionDFS = (edges: [number, number][]): [number, number] => {
   const connectedTo: Record<number, Set<number>> = {};
 
   const touch = (x: number) => {
@@ -40,6 +40,46 @@ export const findRedundantConnection = (edges: [number, number][]): [number, num
       redundants.push([first, second]);
     }
     connect(first, second);
+  }
+
+  return redundants[redundants.length - 1];
+};
+
+export const findRedundantConnectionUnionFind = (edges: [number, number][]): [number, number] => {
+  const sets: Record<number, Set<number>> = {};
+  const redundants: [number, number][] = [];
+
+  for (let edgeIndex = 0; edgeIndex < edges.length; edgeIndex += 1) {
+    const [first, second] = edges[edgeIndex];
+
+    const firstSet = sets[first];
+    const secondSet = sets[second];
+
+    if (!firstSet && !secondSet) {
+      const newSet = new Set<number>();
+      newSet.add(first);
+      newSet.add(second);
+
+      sets[first] = newSet;
+      sets[second] = newSet;
+    } else if (firstSet === secondSet) {
+      redundants.push([first, second]);
+    } else if (firstSet && secondSet) {
+      const firstSet = sets[first];
+      const secondSet = sets[second];
+
+      for (const entry of secondSet) {
+        firstSet.add(entry);
+        sets[entry] = firstSet;
+      }
+      sets[second] = firstSet;
+    } else if (firstSet) {
+      sets[first].add(second);
+      sets[second] = sets[first];
+    } else {
+      sets[second].add(first);
+      sets[first] = sets[second];
+    }
   }
 
   return redundants[redundants.length - 1];
